6297ca2d6b926bb38ac3b275265c6c28f25d0248
Sam McGrath <csammcgrath@gmail.com>
2018-09-24 21:30:38 -0600

    Scratched whole thing and did barebones bootstrap template and will now customize

--- web/header.php
+++ web/header.php
@@ -1,13 +1,21 @@
 <?php
     echo '
-        <header class="masthead mb-auto">
-            <div class="inner">
-                <h3 class="masthead-brand">Sam McGrath</h3>
-                <nav class="nav nav-masthead justify-content-center">
-                    <a class="nav-link active" href="#">Home</a>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="#">Sam McGrath</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="#">Projects</a>
-                </nav>
+                </li>
+                </ul>
             </div>
-        </header>
+        </nav>
     ';
 ?>
--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="shortcut icon" type="image/png" href="https://sylvanson.com/wp-content/uploads/2017/03/favicon-512.png">
 
     <title>Sam McGrath</title>
 
@@ -17,19 +17,18 @@
 
 <body class="text-center">
     <?php include 'header.php' ?>
-
     <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
 
-        <main role="main" class="inner cover">
-            <h1 class="cover-heading">Sam McGrath - CS 313</h1>
-            <p class="lead">Avid software developer, sports fan (golf, baseball and basketball), and lover of the Office</p>
-        </main>
+    <main role="main" class="container">
+      <div class="intro">
+        <h1>Sam McGrath</h1>
+        <p class="lead">Avid software developer, sports fan (golf, baseball and basketball) and lover of the Office./p>
+      </div>
 
-        <?php include 'footer.php' ?>
     </div>
 
+    <?php include 'footer.php' ?>
 
-    <!-- Bootstrap stuff -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
