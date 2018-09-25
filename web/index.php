6f31fec345453f47e043553ab52fe19d1dbd5e69
Sam McGrath <csammcgrath@gmail.com>
2018-09-24 20:56:40 -0600

    Adding cycle.js to local

--- web/index.php
+++ web/index.php
@@ -44,7 +44,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
         crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script type="text/javascript" src="scripts/jquery.cycle.all.2.74.js"></script>
+    <script type="text/javascript" src="scripts/jquery.cycle.lite.js"></script>
     <script src="scripts.js"></script>
 </body>
 
