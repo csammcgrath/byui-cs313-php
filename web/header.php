f3080c0600d3dbdaad51195829d7eb91ba008e2e
Sam McGrath <csammcgrath@gmail.com>
2018-09-25 10:41:46 -0600

    Added extra css styling

--- web/header.php
+++ web/header.php
@@ -5,10 +5,10 @@
 
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
+                <li class="nav-item active link-top-active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item link-top-not-active">
                     <a class="nav-link" href="#">Projects</a>
                 </li>
                 </ul>
