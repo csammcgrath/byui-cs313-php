8099490539a947c81ee47c0394e4d6e7fe63f313
Sam McGrath <csammcgrath@gmail.com>
2018-09-27 16:15:55 -0600

    fixed php

--- web/w02-toa/header.php
+++ web/w02-toa/header.php
@@ -1,19 +1,21 @@
 <?php
-    echo '<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top link-top">
-            <a class="navbar-brand" href="#">Ice Cream Corp.</a>
+    $filename = $_SERVER['REQUEST_URI'];
 
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link <?php if (basename($_SERVER[\'PHP_SELF\'], \'.php\') === \'./about-us.php\') echo \'active\'; ?>" href="./about-us.php">About us</a>
+    echo "<nav class='navbar navbar-expand-md navbar-dark bg-dark fixed-top link-top'>
+            <a class='navbar-brand' href='#'>Ice Cream Corp.</a>
+
+            <div class='collapse navbar-collapse'>
+                <ul class='navbar-nav mr-auto'>
+                    <li class='nav-item'>
+                        <a class='nav-link <?php if ($filename == \'/w02-toa/about-us.php\') echo \'active\'; ?>' href='./about-us.php'>About us</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./home.php">Home</a>
+                    <li class='nav-item'>
+                        <a class='nav-link' href='./home.php'>Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./login.php">Login</a>
+                    <li class='nav-item'>
+                        <a class='nav-link' href='./login.php'>Login</a>
                     </li>
                 </ul>
             </div>
-        </nav>';
+        </nav>";
 ?>
