18bc439bf1403dd74b41e920e8e10042bd89431f
Sam McGrath <csammcgrath@gmail.com>
2018-09-27 15:46:20 -0600

    teach one another - cs313 initial

---
+++ web/w02-toa/header.php
@@ -0,0 +1,19 @@
+<?php
+    echo '<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top link-top">
+            <a class="navbar-brand" href="#">Ice Cream Corp.</a>
+
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About us <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>';
+?>
---
+++ web/w02-toa/login.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Teach One Another">
+    <meta name="author" content="CS 313">
+
+    <title>CS 313 Teach One Another 02</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <link href="... .css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'header.php' ?>
+
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Ice Cream Corp.</h1>
+            <p class="lead"></p>
+        </div>
+    </div>
+
+    <?php include 'footer.php' ?>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
