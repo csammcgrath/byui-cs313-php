05b41ff6e6cee918db3391fd8538d4dfce6c19aa
Sam McGrath <csammcgrath@gmail.com>
2018-09-28 20:18:55 -0600

    Removed footer

--- web/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo '
-        <footer class="footer mt-auto">
-            <div class="inner">
-                <p>Made with ❤️ by Sam McGrath using Bootstrap</p>
-            </div>
-        </footer>
-    ';
-?>
