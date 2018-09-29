a31e63483752f067839bd8c7b69b71041650e6e0
Sam McGrath <csammcgrath@gmail.com>
2018-09-28 20:45:22 -0600

    fixing carousel

--- web/index.php
+++ web/index.php
@@ -17,7 +17,7 @@
 
 <body class="text-center">
     <?php require 'header.php' ?>
-    <div class="theContent cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
         <section class="intro">  
             <div class="content">
                 <h1>Sam McGrath</h1>
