c3814901060aaa01e5d370e8b4851f1f0bb65f4d
Sam McGrath <csammcgrath@gmail.com>
2018-09-28 20:34:07 -0600

    oops. forgot to close off id

--- web/index.php
+++ web/index.php
@@ -22,7 +22,7 @@
             <div class="content">
                 <h1>Sam McGrath</h1>
 
-                <div id="carousel1 class="carousel slide" data-ride="carousel">
+                <div id="carousel1" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img class="d-block w-100" src="./images/main/pic1.jpg" alt="First slide">
