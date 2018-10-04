0d47e9bf637ba4acee7c50e4a1c180a3393f2328
Sam McGrath <csammcgrath@gmail.com>
2018-10-03 23:14:41 -0600

    added no item in cart message conditional

--- web/shopping-cart/checkout.php
+++ web/shopping-cart/checkout.php
@@ -54,34 +54,45 @@
 				<div class="container">
 					<div class="card">
 						<?php
-							foreach ($_SESSION['cart'] as $name => $props) {
-								if ($props['quantity'] == 1) {
-									echo "
-										<div class='card-body'>
-											<div class='row'>
-												<div class='col-12 col-sm-12 col-md-2 text-center'>
-													<img src=" . $props['img'] . " alt=" . $name ." width='120' height='80'>
-												</div>
-												<div class='col-12 text-sm-center col-sm-12 text-md-left col-md-6'>
-													<h4 class='product-name'>" . $props['name'] . "</h4>
-												</div>
-												<div class='col-12 col-sm-12 text-sm-center col-md-4 text-md-right row'>
-													<div class='col-3 col-sm-3 col-md-6 text-md-right m-auto'>
-														<h6>$" . $props['price'] ."</h6>
+							if (sizeof($_SESSION['cart']) >= 1) {
+								foreach ($_SESSION['cart'] as $name => $props) {
+									if ($props['quantity'] == 1) {
+										echo "
+											<div class='card-body'>
+												<div class='row'>
+													<div class='col-12 col-sm-12 col-md-2 text-center'>
+														<img src=" . $props['img'] . " alt=" . $name ." width='120' height='80'>
 													</div>
-													<div class='col-2 col-sm-2 col-md-2 text-right m-auto'>
-														<form method='post'>
-															<button type='submit' name='" . $name . "' class='btn btn-outline-danger btn-xs'>
-																<i class='fas fa-trash'></i>
-															</button>
-														</form>
+													<div class='col-12 text-sm-center col-sm-12 text-md-left col-md-6'>
+														<h4 class='product-name'>" . $props['name'] . "</h4>
+													</div>
+													<div class='col-12 col-sm-12 text-sm-center col-md-4 text-md-right row'>
+														<div class='col-3 col-sm-3 col-md-6 text-md-right m-auto'>
+															<h6>$" . $props['price'] ."</h6>
+														</div>
+														<div class='col-2 col-sm-2 col-md-2 text-right m-auto'>
+															<form method='post'>
+																<button type='submit' name='" . $name . "' class='btn btn-outline-danger btn-xs'>
+																	<i class='fas fa-trash'></i>
+																</button>
+															</form>
+														</div>
 													</div>
 												</div>
 											</div>
-										</div>
-										<br>
-									";
+											<br>
+										";
+									}
 								}
+							} else {
+								echo "
+									<div class='card-body'>
+										<div class='row'>
+											<h1>No items in cart.</h1>
+										</div>
+									</div>
+									<br>
+								";
 							}
 						?>
 						<div class="container">
