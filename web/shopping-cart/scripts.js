5ae387d58996ac0e2942646924d5c9dc8c8652a2
Sam McGrath <csammcgrath@gmail.com>
2018-10-06 15:09:42 -0600

    fixed bug

--- web/shopping-cart/scripts.js
+++ web/shopping-cart/scripts.js
@@ -1,5 +1,5 @@
 window.setTimeout(function () {
-	$("#alert_message").fadeTo(500, 0).slideUp(500, function () {
+	$("#alert_message").fadeTo(500, 0).slideUp(500, () => {
 		$(this).remove();
 	});
 }, 3000);
