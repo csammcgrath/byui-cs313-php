bd797c7519bb416f6e1598aa5780c4c6885594fd
Sam McGrath <csammcgrath@gmail.com>
2018-10-20 14:32:56 -0600

    testing home page

--- db/myDb.sql
+++ db/myDb.sql
@@ -6,7 +6,7 @@ CREATE TABLE users
 (
     id              SERIAL              PRIMARY KEY,
     username        VARCHAR(50)         UNIQUE NOT NULL,
-    password        VARCHAR(40)         NOT NULL
+    password        VARCHAR(255)        NOT NULL
 );
 
 CREATE TABLE blog_post 
@@ -24,6 +24,17 @@ CREATE TABLE comment
     comment         VARCHAR(250)        NOT NULL
 );
 
+-- QUERIES
+-- SELECT bp.title, c.comment, u.username FROM blog_post bp
+--     JOIN comment c
+--         ON c.blogId = bp.id
+--     JOIN users u
+--         ON u.id = bp.userId;
+
+-- SELECT title from blog_post
+-- WHERE title LIKE 'Lorem Ipsum 1%'
+-- ORDER BY title DESC;
+
 INSERT INTO users (username, password) VALUES
 (
     'sammcgrath67',
@@ -35,3 +46,27 @@ INSERT INTO users (username, password) VALUES
     'blogger',
     'blog1'
 );
+
+INSERT INTO blog_post (userId, title, body) VALUES
+(1, 'Lorem Ipsum 1 First', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et quam consectetur dolor egestas rhoncus. Suspendisse aliquam blandit condimentum. Quisque elementum consequat mauris ac condimentum. Cras egestas convallis mattis. Nunc sit amet erat neque. Integer lobortis, ligula ac suscipit feugiat, mi arcu placerat nunc, non lacinia felis nulla id nulla. Donec gravida, quam sit amet convallis imperdiet, tortor quam euismod nibh, vitae sagittis orci lorem in nunc. Nam non augue vulputate, feugiat sem a, gravida tortor. Fusce ornare eros vitae scelerisque eleifend. Sed a erat ac odio varius aliquet. Suspendisse nisl metus, venenatis quis mauris nec, pretium tincidunt velit. Donec et tortor quis lacus scelerisque eleifend at sodales felis. Vivamus non risus tortor. Sed lacinia justo vulputate, viverra massa a, iaculis lacus. Duis at neque vitae tellus luctus porttitor. Praesent condimentum sed justo vulputate dignissim. Quisque justo elit, scelerisque non mollis vel, fermentum tincidunt risus. Sed in venenatis arcu. Nulla condimentum aliquet nibh, eget eleifend metus finibus a. Praesent et placerat felis. Sed sit amet urna sagittis, consectetur massa in, luctus felis. Donec tincidunt tellus non lacus dapibus, lacinia rutrum diam dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra convallis risus, quis rutrum magna vehicula et. Nam scelerisque fermentum lectus in posuere. Phasellus sollicitudin magna eget metus aliquam hendrerit. In porta pulvinar erat at ultricies. Proin nec volutpat lorem. Aenean nulla odio, accumsan at tellus eget, bibendum dictum dui. Pellentesque ligula est, congue placerat dignissim vitae, suscipit nec dui. Nullam nec est sed justo convallis dignissim. Nam vel condimentum dui. Praesent consequat convallis erat, ac ullamcorper nisl. Mauris suscipit, quam vitae auctor mollis, nisl felis bibendum massa, sed iaculis magna purus id nisi. Nunc urna arcu, congue non dolor sit amet, placerat placerat purus. Mauris sapien tellus, ullamcorper et dolor et, pharetra vehicula justo. Integer auctor suscipit eleifend. Fusce vestibulum purus ut lacus hendrerit ullamcorper. In hac habitasse platea dictumst. Donec rutrum lectus sit amet malesuada sagittis. Vestibulum molestie lectus metus. Etiam ullamcorper quam ex, at vulputate turpis semper vitae. Quisque commodo sagittis nunc dictum commodo. Aenean semper nibh eu nulla auctor, vitae porttitor turpis convallis.');
+
+INSERT INTO blog_post (userId, title, body) VALUES
+(2,  'Lorem Ipsum 2 First', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et quam consectetur dolor egestas rhoncus. Suspendisse aliquam blandit condimentum. Quisque elementum consequat mauris ac condimentum. Cras egestas convallis mattis. Nunc sit amet erat neque. Integer lobortis, ligula ac suscipit feugiat, mi arcu placerat nunc, non lacinia felis nulla id nulla. Donec gravida, quam sit amet convallis imperdiet, tortor quam euismod nibh, vitae sagittis orci lorem in nunc. Nam non augue vulputate, feugiat sem a, gravida tortor. Fusce ornare eros vitae scelerisque eleifend. Sed a erat ac odio varius aliquet. Suspendisse nisl metus, venenatis quis mauris nec, pretium tincidunt velit. Donec et tortor quis lacus scelerisque eleifend at sodales felis. Vivamus non risus tortor. Sed lacinia justo vulputate, viverra massa a, iaculis lacus. Duis at neque vitae tellus luctus porttitor. Praesent condimentum sed justo vulputate dignissim. Quisque justo elit, scelerisque non mollis vel, fermentum tincidunt risus. Sed in venenatis arcu. Nulla condimentum aliquet nibh, eget eleifend metus finibus a. Praesent et placerat felis. Sed sit amet urna sagittis, consectetur massa in, luctus felis. Donec tincidunt tellus non lacus dapibus, lacinia rutrum diam dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra convallis risus, quis rutrum magna vehicula et. Nam scelerisque fermentum lectus in posuere. Phasellus sollicitudin magna eget metus aliquam hendrerit. In porta pulvinar erat at ultricies. Proin nec volutpat lorem. Aenean nulla odio, accumsan at tellus eget, bibendum dictum dui. Pellentesque ligula est, congue placerat dignissim vitae, suscipit nec dui.');
+
+INSERT INTO blog_post (userId, title, body) VALUES
+(1, 'Lorem Ipsum 1 Second', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et quam consectetur dolor egestas rhoncus. Suspendisse aliquam blandit condimentum. Quisque elementum consequat mauris ac condimentum. Cras egestas convallis mattis. Nunc sit amet erat neque. Integer lobortis, ligula ac suscipit feugiat, mi arcu placerat nunc, non lacinia felis nulla id nulla. Donec gravida, quam sit amet convallis imperdiet, tortor quam euismod nibh, vitae sagittis orci lorem in nunc. Nam non augue vulputate, feugiat sem a, gravida tortor. Fusce ornare eros vitae scelerisque eleifend. Sed a erat ac odio varius aliquet. Suspendisse nisl metus, venenatis quis mauris nec, pretium tincidunt velit. Donec et tortor quis lacus scelerisque eleifend at sodales felis. Vivamus non risus tortor. Sed lacinia justo vulputate, viverra massa a, iaculis lacus. Duis at neque vitae tellus luctus porttitor.');
+
+INSERT INTO comment (blogId, comment) VALUES
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+
+INSERT INTO comment (blogId, comment) VALUES
+(1, 'Lorem ipsum dolor sit amet.');
+
+INSERT INTO comment (blogId, comment) VALUES
+(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla felis, dignissim nec consequat sed.');
+
+INSERT INTO comment (blogId, comment) VALUES
+(3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+
+INSERT INTO comment (blogId, comment) VALUES
+(3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla felis.');
---
+++ web/blog/index.php
@@ -0,0 +1,100 @@
+<?php
+  require('dbConnect.php');
+  $db = get_db();
+
+  $stmt = $db->prepare('SELECT bp.title, bp.body, c.comment, u.username FROM blog_post bp
+                            JOIN comment c
+                                ON c.blogId = bp.id
+                            JOIN users u
+                                ON u.id = bp.userId;');
+
+  $stmt->execute();
+  $blogs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Blog">
+    <meta name="author" content="Sam McGrath">
+
+    <title>Simple dev.to clone</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <link href="styles.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Simple dev.to clone</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1 class="my-4">Posts</h1>
+          <hr style="width:80%;margin-left:0px;"/>
+          <?php
+            foreach($blogs as $blogPost) {
+              $title = $blogPost['title'];
+              $body = substr($blogPost['body'], 0, 30);
+              $comments = $blogPost['comment'];
+
+              echo "
+                <div class='card mb-4'>
+                  <div class='card-body'>
+                    <h2 class='card-title'>$title</h2>
+                    <p class='card-text'>$body</p>
+                    <button class='btn-hover float-right'><span>Read</span></button>
+                  </div>
+                </div>
+              ";
+            }
+          ?>
+
+        <div class="col-md-4">
+          <div class="card my-4">
+            <h5 class="card-header">Search</h5>
+            <div class="card-body">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search for name...">
+                <span class="input-group-btn">
+                  <button class="btn btn-secondary" type="button">Search</button>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="./scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+  </body>
+
+</html>
