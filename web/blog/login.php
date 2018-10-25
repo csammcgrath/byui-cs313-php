fd45487758e984d61a2d705005fa794d332e7343
Sam McGrath <csammcgrath@gmail.com>
2018-10-25 11:13:59 -0600

    fixing create account button

--- web/blog/login.php
+++ web/blog/login.php
@@ -87,7 +87,9 @@
                 </div>
             </div>
       </form>
-      <a href="signUp.php" class="text-center" style="text-decoration:underline;">Don't have an account?</a>
+      <div class="row">
+          <a href="signUp.php" class="text-center" style="text-decoration:underline;">Don't have an account?</a>
+      </div>
     </div>
 
    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
