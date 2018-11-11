cc152785e5b87e4347ab609c5efa97785fb60f60
Sam McGrath <csammcgrath@gmail.com>
2018-11-10 18:41:13 -0700

    pushing final changes

--- web/node-server/index.js
+++ web/node-server/index.js
@@ -17,6 +17,30 @@ http.createServer((request, response) => {
             'name': 'Br. Burton',
             'class': 'CS313'
         }));
+    } else if (userUrl.pathname === '/retrieveBasicSum') {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+
+        response.write(JSON.stringify({
+            'firstValue': 2,
+            'secondValue': 2,
+            'total': 2 + 2
+        }));
+    } else if (userUrl.pathname === '/sumValues') {
+        let numbers = userUrl.query;
+
+        if (Object.keys(numbers).length === 0) {
+            response.writeHead(404, { 'Content-Type': 'text/html' });
+
+            response.write('<h1>Page not found!</h1>');
+        } else {
+            let sum = Object.values(numbers)[0].reduce((total, number) => total += parseInt(number), 0);
+
+            response.writeHead(200, {'Content-Type': 'application/json'});
+
+            response.write(JSON.stringify({
+                'Total': sum
+            }));
+        }
     } else {
         response.writeHead(404, {'Content-Type': 'text/html'});
 
